aeff5b609d254eb3556bbc3e2849eea4715ebc57
Alex Groff <agroff1201@gmail.com>
2026-06-01 17:55:42 -0700

    Initial Commit

---
+++ Assets/Map Gen.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MapGen : MonoBehaviour
+{
+
+    public Grid layout;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void GenMap()
+    {
+        
+    }
+}
